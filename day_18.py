f9ea25db18ed2a92ae0de5860741b78ad09f0478
shanelle-hub <njingtishanelle@gmail.com>
2025-01-07 15:49:44 +0100

    function to rotate array

--- day_18.py
+++ day_18.py
@@ -1,54 +1,24 @@
 import array
 
+# Create the array
 a = array.array("i", [1, 2, 3])
-b = array.array("i", [11, 12, 13])
-a.insert(4, 4)
-a.index(1)
-a += b
-count = a.count(1)
+
+# Insert 4 at the end (corrected index)
+a.append(4)
+
+
+# Extend the array
 a.extend([5, 6, 7, 8, 9, 10])
-print(a)
+print("extended array", a)
 
 
-def swap(arr, n):
-    temp = arr[0]
-    for i in range(n - 1):
-        arr[i], arr[i + 1] = arr[i + 1], arr[i]
-        arr[n - 1] = temp
-        i += 1
+# Function to rotate the array with a while loop
+def rotate_array(arr):
+    n = len(arr)
+    k = 3
+    while k<n:
+        arr = arr[n - k :] + arr[: n - k]  
+        print(f"Rotation {k + 1}: {arr}")  
+        k += 1  
 
-
-swap(a, 5)
-print(a)
-
-
-def reverse_array(end, start, arr):
-    reverse_order = end - start + 1
-    count = 0
-    while (reverse_order) // 2 != count:
-        arr[start + count], arr[end - count] = arr[end - count], arr[start + count]
-        count += 1
-        return arr
-
-
-def rotate_arr(arr, size, d):
-    start = 0
-    end = size - 1
-    arr = reverse_array(start, end, arr)
-
-    start = size - d
-    end = size - 1
-    arr = reverse_array(start, end, arr)
-    return arr
-
-
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-size = 8
-d = 1
-print(arr)
-
-if d <= size:
-    print(rotate_arr(arr, size, d))
-else:
-    d = d % size
-    print(rotate_arr(arr, size, d))
+rotate_array(a)
